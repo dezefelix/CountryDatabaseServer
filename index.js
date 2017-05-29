6e23b286671c49d8df847699cc9c19a5307c36d2
Felix Boons <felixboons@hotmail.com>
2017-05-29 17:22:57 +0200

    extended-app-file

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
  * Created by Felix on 29-5-2017.
  */
 
+//core modules
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 
+//my modules
+var config = require('./config.json');
+
+//create application
 var app = express();
+
+app.set('PORT', config.webPort);
+app.set('SECRET_KEY', config.secretKey);
+
+var port = process.env.PORT || app.get('PORT');
 
